31df76882fcfbc8c826f534889c9039119cc6b88
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-25 18:21:13 +0600

    create courses and course detail API

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const port = process.env.PORT || 5002;
 const cors = require('cors');
 app.use(cors());
 
+const courses = require('./data/courses.json');
+const courseDetails = require('./data/courseDetails.json');
+
 app.get('/', (req, res) =>{
     res.send('testing the server')
 });
 
+app.get('/courses/', (req, res) => {
+    res.send(courses)
+});
+
+app.get('/course-details/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find(detail => detail.id === id);
+    res.send(selectedCourse);
+})
 
 
 
